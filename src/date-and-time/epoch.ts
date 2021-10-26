e564a9a09dbc5081f392f5a5ba4932d9a008db7c
minefled <florian-martin@stembal.de>
2021-10-26 21:47:47 +0200

    create epoch and now functions

---
+++ src/date-and-time/epoch.ts
@@ -0,0 +1,10 @@
+/**
+ * Returns the Epoch Date
+ * 
+ * @since 0.1
+ * 
+ * @returns {Date} The start of the Unix time epoch
+ */
+export function epoch() {
+    return new Date(0);
+}
---
+++ src/date-and-time/now.ts
@@ -0,0 +1,10 @@
+/**
+ * Returns the current Date
+ * 
+ * @since 0.1
+ * 
+ * @returns {Date}
+ */
+export function now() {
+    return new Date();
+}
