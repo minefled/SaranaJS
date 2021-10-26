cfe0bb2188cd63fe5db40bbfd6771d27774f428b
minefled <florian-martin@stembal.de>
2021-10-26 21:52:16 +0200

    create isNumber and isString functions

---
+++ src/types/isNumber.ts
@@ -0,0 +1,16 @@
+/**
+ * Returns true if the entered variable is a number
+ * 
+ * @since 0.1
+ * 
+ * @param {*} value The value you want to check
+ * 
+ * @example
+ *      isNumber(0)         -> true
+ *      isNumber(Infinity)  -> true
+ *      isNumber("foo")     -> false
+ *      isNumber(null)      -> false
+ */
+export function isNumber(value:any):boolean {
+    return typeof value === "number";
+}
---
+++ src/types/isString.ts
@@ -0,0 +1,17 @@
+/**
+ * Returns true if the value is a string
+ * 
+ * @since 0.1
+ * 
+ * @param {any} value The value you want to check
+ * 
+ * @example
+ *      isString("Hello World") -> true
+ *      isString(0)             -> false
+ *      isString(undefined)     -> false
+ *      isString("foo")         -> true
+ */
+
+export function isString(value:any):boolean {
+    return (typeof value === "string");
+}
