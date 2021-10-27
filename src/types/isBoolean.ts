f95d605491a5aa273637b9c26b20a7451145ef36
minefled <florian-martin@stembal.de>
2021-10-27 14:26:25 +0200

    added isBoolean function

---
+++ src/types/isBoolean.ts
@@ -0,0 +1,17 @@
+/**
+ * Returns true if the value is a boolean
+ * 
+ * @since 0.1
+ * 
+ * @param {any} value The value you want to check
+ * 
+ * @example
+ *      isBoolean("Hello World")    -> false
+ *      isBoolean(true)             -> true
+ *      isBoolean(false)            -> true
+ *      isBoolean(0)                -> false
+ */
+
+ export function isBoolean(value:any):boolean {
+    return (typeof value === "boolean");
+}
