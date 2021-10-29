be6502437a649b75d8ff23e18cfbb7ac92dc8ec2
minefled <florian-martin@stembal.de>
2021-10-29 14:49:03 +0200

    create isInstanceOfFunction

---
+++ src/types/isInstanceOf.ts
@@ -0,0 +1,23 @@
+import { isFunction } from "./isFunction.js";
+import { isObject } from "./isObject.js";
+
+/**
+ * Returns true if the object is instance of the instance
+ * 
+ * @param {Object} obj The object you want to the the instance check on
+ * @param {Function} instance 
+ * 
+ * @example
+ * 
+ *      class A {}
+ *      let b = new A();
+ * 
+ *      isInstanceOf(b, A);     -> true
+ * 
+ */
+export function isInstanceOf(obj:Object, instance:Function):boolean {
+    if(!isObject(obj)) return false;
+    if(!isFunction(instance)) return false;
+
+    return (obj instanceof instance);
+}
