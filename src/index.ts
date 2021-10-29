06bdf0f51f76df3e634e8e138873328dacb9beb8
minefled <florian-martin@stembal.de>
2021-10-29 14:52:11 +0200

    implemented isFunction & isInstanceOf functions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { isBoolean } from "./types/isBoolean.js";
 import { isNull } from "./types/isNull.js";
 import { isUndefined } from "./types/isUndefined.js";
 import { isObject } from "./types/isObject.js";
+import { isFunction } from "./types/isFunction.js";
+import { isInstanceOf } from "./types/isInstanceOf.js";
 
 import { now } from "./date-and-time/now.js";
 import { epoch } from "./date-and-time/epoch.js";
@@ -15,6 +17,9 @@ export const sarana = {
     isNull,
     isUndefined,
     isObject,
+    isFunction,
+
+    isInstanceOf,
 
     now,
     epoch
