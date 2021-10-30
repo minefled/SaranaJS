03954a03e76340692a01e5bf4ae1d3634c52bfef
minefled <florian-martin@stembal.de>
2021-10-30 20:30:01 +0200

    update concat function

--- src/array/concat.ts
+++ src/array/concat.ts
@@ -1,1 +1,20 @@
-export let concat = Array.prototype.concat;
+import { isArray } from "../types/isArray.js";
+
+/**
+ * Concatenates all items to one array
+ * 
+ * @example
+ *      concat(["a", "b", "c"], [1, 2, 3], "hey") -> ["a", "b", "c", 1, 2, 3, "hey"]
+ */
+export function concat(...items:any[]):Array<any> {
+    let res = [];
+
+    for(var i=0; i<arguments.length; i++) {
+        var arg = arguments[i];
+
+        if(isArray(arg)) res.push(...arg);
+        else             res.push(arg);
+    }
+
+    return res;
+}
